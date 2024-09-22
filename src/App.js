61395bd9841bb9f60352d4be17eedc2c18158dc4
jmLecias <johnmarklecias.lsd@gmail.com>
2024-09-22 21:50:34 +0800

    modular css and attendance list fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/App.css';
+// import './css/App.css'; // Transfered all css in their own modules
+import './css/variables.css'
+import './css/header-styles.css'
+import './css/sidebar-styles.css'
+import './css/feed-styles.css'
+import './css/item-styles.css'
+import './css/main-grid-styles.css'
+import './css/landing-page-styles.css'
+import './css/auth-page-styles.css'
+import './css/location-page-styles.css'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import AuthPage from './pages/auth/AuthPage';
